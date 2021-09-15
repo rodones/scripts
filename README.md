7adfc682174d9ad35df18ed9b64a7c323fd4284a
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-09-15 17:07:36 +0300

    vid2img: generate input images from video

--- README.md
+++ README.md
@@ -14,5 +14,11 @@ This repository contains custom scripts, docker files, etc.
 
       ./start.sh WORKSPACE [--gpu | --cpu]
 
-- Execute scripts using rodo in the container.
+- Execute scripts using `rodo` in the container.
   - Type `rodo --help` to see available scripts.
+
+## Video to Image Convertion
+
+- Install `ffmpeg`.
+
+- Run `vid2img.sh` with input file and output folder, and optionally fps (default is 1 which means 1 frame per 1 second). (Example `./vid2img.sh ./VID_20210910_093215.mp4 ./images/ 5`)
---
+++ vid2img.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+FPS_DEFAULT="60/60"
+OUTPUT_BASENAME="P_%04d.png"
+
+INPUT_FILE="$1"
+OUTPUT_DIR="$2"
+FPS="${3:-$FPS_DEFAULT}"
+
+print_help() {
+    echo "Usage: $(basename "$0") INPUT_FILE OUTPUT_DIR [FPS=$FPS_DEFAULT]"
+}
+
+[ "$1" = "--help" ] && print_help && exit
+
+if [ -z "$INPUT_FILE" ]; then
+    echo "error: input file is not specified."
+    exit 1
+elif [ ! -f "$INPUT_FILE" ]; then
+    echo "error: input file does not exist."
+    exit 2
+fi
+
+if [ -z "$OUTPUT_DIR" ]; then
+    echo "error: output directory is not specified."
+    exit 1
+elif [ ! -d "$OUTPUT_DIR" ]; then
+    echo "error: output directory does not exist."
+    exit 2
+fi
+
+INPUT_BASENAME="$(basename "$INPUT_FILE")"
+
+if [[ "$INPUT_BASENAME" =~ ^VID_[0-9]{8}_[0-9]{6} ]]; then
+    OUTPUT_BASENAME="P_${INPUT_BASENAME:4:15}_%04d.png"
+fi
+
+ffmpeg -i "$INPUT_FILE" -vf fps="$FPS" "$OUTPUT_DIR/$OUTPUT_BASENAME"
