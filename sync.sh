2881c5d169ec02ced548ebdc99dfaa94b41ac229
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-10-10 12:01:51 +0300

    sync: merge two variant

---
+++ sync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+REMOTE_ROOT="s3://rodones"
+
+if [ "$1" = "up" ]; then
+    FROM="${2%/}/"
+    TO="$REMOTE_ROOT/$(basename "$FROM")"
+elif [ "$1" = "down" ]; then
+    TO="${2%/}/"
+    FROM="$REMOTE_ROOT/$(basename "$TO")"
+else
+    echo "error: found $1, expected: up, down."
+    exit
+fi
+
+echo "Are you sure to update '$TO' according to '$FROM'?"
+select yn in "Yes" "No"; do
+    case $yn in
+    Yes)
+        s3cmd sync --no-preserve "$FROM" "$TO"
+        break
+        ;;
+    No)
+        exit
+        ;;
+    esac
+done
