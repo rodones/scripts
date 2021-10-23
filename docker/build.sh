973f96f252b47f1351227014a1bf0ba445553079
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-10-23 21:34:09 +0300

    docker: use multi-stage build

--- docker/build.sh
+++ docker/build.sh
@@ -11,18 +11,24 @@ VERSION="latest"
 
 CUDA_ENABLED=""
 
+BASE_IMAGE_BUILD=""
+BASE_IMAGE_BUILD_CUDA="nvidia/cuda:10.2-devel-ubuntu18.04"
+BASE_IMAGE_BUILD_WITHOUT_CUDA="ubuntu:bionic"
+
 BASE_IMAGE=""
-BASE_IMAGE_CUDA="nvidia/cuda:10.2-devel-ubuntu18.04"
+BASE_IMAGE_CUDA="nvidia/cuda:10.2-runtime-ubuntu18.04"
 BASE_IMAGE_WITHOUT_CUDA="ubuntu:bionic"
 
 if [ "$1" = "--with-cuda" ]; then
     CUDA_ENABLED="ON"
     VERSION="gpu-$VERSION"
     BASE_IMAGE="$BASE_IMAGE_CUDA"
+    BASE_IMAGE_BUILD="$BASE_IMAGE_BUILD_CUDA"
 elif [ "$1" = "--without-cuda" ]; then
     CUDA_ENABLED="OFF"
     VERSION="cpu-$VERSION"
     BASE_IMAGE="$BASE_IMAGE_WITHOUT_CUDA"
+    BASE_IMAGE_BUILD="$BASE_IMAGE_BUILD_WITHOUT_CUDA"
 else
     echo "error: please specify build type with --with-cuda or --without-cuda."
     exit 1
@@ -33,5 +39,6 @@ docker build \
     --build-arg GID="$(id -g)" \
     --build-arg CUDA_ENABLED="$CUDA_ENABLED" \
     --build-arg BASE_IMAGE="$BASE_IMAGE" \
+    --build-arg BASE_IMAGE_BUILD="$BASE_IMAGE_BUILD" \
     -t "$ORG/$NAME:$VERSION" \
     "$SCRIPT_DIR"
