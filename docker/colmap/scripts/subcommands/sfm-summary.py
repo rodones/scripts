f45cf945bd5b670818b48c72f774ae45618347eb
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-11-16 12:30:09 +0300

    sfm: print summary instead of writing file

--- docker/colmap/scripts/subcommands/sfm-summary.py
+++ docker/colmap/scripts/subcommands/sfm-summary.py
@@ -2,7 +2,6 @@
 
 from os.path import join
 import sqlite3
-from datetime import datetime
 
 BASE_DIR = join("/", "working")
 LOGS_DIR = join(BASE_DIR, "logs")
@@ -11,24 +10,18 @@ DB_PATH = join(BASE_DIR, "output", "database.db")
 
 def prepare_sfm_keypoints():
     db = sqlite3.connect(DB_PATH)
-    today = datetime.now()
-    date_str = "{}-{}-{}_{}-{}-{}".format(today.year, today.month,
-                                          today.day, today.hour,
-                                          today.minute, today.second)
-    log_file = open(join(LOGS_DIR, f"sfm_{date_str}.txt"), "w")
 
-    log_file.write("{}\t{}\t{}\n"
-                   .format("image_id", "image_name".ljust(30), "keypoints"))
+    print("{}\t{}\t{}\n"
+          .format("image_id", "image_name".ljust(30), "keypoints"))
     for image_id, name, data in db.execute(
             "SELECT kp.image_id, imgs.name, kp.rows " +
             "FROM keypoints as kp " +
             "JOIN images as imgs " +
             "ON imgs.image_id = kp.image_id " +
             "ORDER BY kp.rows"):
-        log_file.write("{: 6}\t{}\t{}\n"
-                       .format(image_id, name.ljust(30), data))
+        print("{: 6}\t{}\t{}\n"
+              .format(image_id, name.ljust(30), data))
 
-    log_file.close()
     db.close()
 
 
