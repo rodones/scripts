fd00b47b023cbe5a069f064ddb24edcc4875ee5c
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-11-16 13:09:15 +0300

    sfm: refactor summary

--- docker/colmap/scripts/subcommands/sfm-summary.py
+++ docker/colmap/scripts/subcommands/sfm-summary.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from os.path import join
+from sys import stdout
 import sqlite3
 
 BASE_DIR = join("/", "working")
@@ -11,19 +12,23 @@ DB_PATH = join(BASE_DIR, "output", "database.db")
 def prepare_sfm_keypoints():
     db = sqlite3.connect(DB_PATH)
 
-    print("{}\t{}\t{}\n".format("image_id", "image_name".ljust(30), "keypoints"),
-          flush=True)
+    query = "SELECT kp.image_id, imgs.name, kp.rows " \
+        "FROM keypoints as kp " \
+        "JOIN images as imgs ON imgs.image_id = kp.image_id " \
+        "ORDER BY kp.rows"
+
+    stdout.write("{}\t{}\t{}\n".format("image_id", "image_name".ljust(30), "keypoints"))
     try:
-        for image_id, name, data in db.execute(
-                "SELECT kp.image_id, imgs.name, kp.rows " +
-                "FROM keypoints as kp " +
-                "JOIN images as imgs " +
-                "ON imgs.image_id = kp.image_id " +
-                "ORDER BY kp.rows"):
-            print("{: 6}\t{}\t{}".format(image_id, name.ljust(30), data),
-                  flush=True)
+        for image_id, name, data in db.execute(query):
+            stdout.write("{: 6}\t{}\t{}\n".format(image_id, name.ljust(30), data))
     except (BrokenPipeError, IOError):
         pass
+
+    try:
+        stdout.close()
+    except IOError:
+        pass
+
     db.close()
 
 
