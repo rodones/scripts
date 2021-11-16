9458c2116fe968cb030bea2e48e473d69e464c0e
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2021-11-16 12:43:27 +0300

    sfm: fix broken pipe error

--- docker/colmap/scripts/subcommands/sfm-summary.py
+++ docker/colmap/scripts/subcommands/sfm-summary.py
@@ -11,17 +11,19 @@ DB_PATH = join(BASE_DIR, "output", "database.db")
 def prepare_sfm_keypoints():
     db = sqlite3.connect(DB_PATH)
 
-    print("{}\t{}\t{}\n"
-          .format("image_id", "image_name".ljust(30), "keypoints"))
-    for image_id, name, data in db.execute(
-            "SELECT kp.image_id, imgs.name, kp.rows " +
-            "FROM keypoints as kp " +
-            "JOIN images as imgs " +
-            "ON imgs.image_id = kp.image_id " +
-            "ORDER BY kp.rows"):
-        print("{: 6}\t{}\t{}"
-              .format(image_id, name.ljust(30), data))
-
+    print("{}\t{}\t{}\n".format("image_id", "image_name".ljust(30), "keypoints"),
+          flush=True)
+    try:
+        for image_id, name, data in db.execute(
+                "SELECT kp.image_id, imgs.name, kp.rows " +
+                "FROM keypoints as kp " +
+                "JOIN images as imgs " +
+                "ON imgs.image_id = kp.image_id " +
+                "ORDER BY kp.rows"):
+            print("{: 6}\t{}\t{}".format(image_id, name.ljust(30), data),
+                  flush=True)
+    except BrokenPipeError:
+        pass
     db.close()
 
 
