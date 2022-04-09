f964370d64fe0222d3cc7d44fabe6cdef4cbfca7
Gökberk AKDENİZ <gokberkakdeniz@hotmail.com>
2022-04-09 17:56:30 +0300

    fix: update to new bucket

--- activate.sh
+++ activate.sh
@@ -8,7 +8,7 @@ SCRIPT_DIR="$(
 # Load environment variables
 # TODO: handle hashtags
 # shellcheck disable=SC2046
-export $(grep -e AWS_ -e S3_ "$SCRIPT_DIR/.env" | xargs)
+export $(grep -e AWS_ -e S3_ ".env" | xargs)
 export RODONES_ENV=1
 
 alias s4cmd='s4cmd --endpoint-url $S3_ENDPOINT_URL'
@@ -17,7 +17,7 @@ PS1="(rodones) $PS1"
 
 rodo_next_index() {
     local current_index
-    current_index=$(s4cmd ls "s3://rodones-images/$1/$1_*" | grep -Po "(?<=$1_)([0-9]+)(?=_)" | sort -n | tail -n1)
+    current_index=$(s4cmd ls "s3://rodones-images2/$1/$1_*" | grep -Po "(?<=$1_)([0-9]+)(?=_)" | sort -n | tail -n1)
 
     [ -z "$current_index" ] && current_index=0
     current_index=$((current_index + 1))
